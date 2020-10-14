95201c641a46cbf91c16a0af96427e4ddfde5494
zfima <zzfima@gmail.com>
2020-10-14 15:02:44 +0300

    init

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
+import requests
+import extruct
+from w3lib.html import get_base_url
 
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-}
 
-url = "http://example.com"
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-print(soup.prettify())
+def get_html(url):
+    """Get raw HTML from a URL."""
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+    req = requests.get(url, headers=headers)
+    return req.text
+
+
+def scrape(url):
+    """Parse structured data from a target page."""
+    html = get_html(url)
+    metadata = get_metadata(html, url)
+    pprint(metadata, indent=2, width=150)
+    return metadata
+
+
+def get_metadata(html, url):
+    """Fetch JSON-LD structured data."""
+    metadata = extruct.extract(
+        html,
+        base_url=get_base_url(html, url),
+        syntaxes=['json-ld'],
+    )['json-ld'][0]
+    return metadata
+
+
+print(scrape("https://hackersandslackers.com/"))
